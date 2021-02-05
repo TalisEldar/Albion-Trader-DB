c4f85322ab3b0316a3a59b8b605de75f3a8a015c
TalisEldar <cihan.akinci@gmail.com>
2021-02-05 15:46:48 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#Coordinates of Message Box in game
+#To-Do: need to do parametric
+mbox =(1007, 258, 1549, 1179)
+
+#speedofmouse
+ms=1
+
+#message ccordinates
+msgcoordinates=((1265,534,ms), (1265,606,ms),(1265,678,ms),(1265,750,ms),(1265,822,ms),(1265,894,ms),(1265,966,ms),(1265,1038,ms))
+
